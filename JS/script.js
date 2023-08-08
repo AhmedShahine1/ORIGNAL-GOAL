6101cfca25d5ae634b7fd1103ce808b5552519e8
AhmedShahine1 <ahmedshaheen748@gmail.com>
2023-08-08 06:14:36 +0300

    frontend

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+
+// var divToToggle = document.querySelector('.page');
+
+// // Hide the div initially
+// divToToggle.classList.add('hidden');
+
+// setTimeout(function() {
+//   divToToggle.classList.remove('hidden');
+//   var divToDelete = document.querySelector('.text-container');
+//   divToDelete.remove();
+// }, 11000);
+
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
